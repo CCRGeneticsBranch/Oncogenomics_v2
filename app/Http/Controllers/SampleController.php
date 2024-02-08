6ac74fd13397ecc6029d696c15a7a08e64050013
Chou <chouh@fsabcl-onc04p.ncifcrf.gov>
2024-02-08 12:21:20 -0500

    fixed gene level not display issue

--- app/Http/Controllers/SampleController.php
+++ app/Http/Controllers/SampleController.php
@@ -332,12 +332,6 @@ class SampleController extends BaseController {
 						$cnv_genelevel_samples[$sample->sample_name] = $sample->case_id;
 					}
 					$glsamples = VarAnnotation::getCNVGeneLevelSamples($patient_id, $sample->case_id, $sample->sample_id, "cnvkit");
-					if (count($glsamples) > 0) {
-						$cnvkit_genelevel_samples[$sample->sample_name] = $sample->case_id;
-					}
-
-
-
 					$file = storage_path()."/ProcessedResults/".$sample->path."/$patient_id/$sample->case_id/$sample->sample_name/cnvkit/$sample->sample_name".".pdf";
 					Log::info("====== CNVkit file: $file");
 					if (!file_exists($file)) {
@@ -346,14 +340,25 @@ class SampleController extends BaseController {
 							$file = storage_path()."/ProcessedResults/".$sample->path."/$patient_id/$sample->case_id/Sample_$sample->sample_id/cnvkit/Sample_$sample->sample_id".".pdf";
 							if (!file_exists($file))
 								Log::info("no CNVkit!");
-							else
+							else {
 								$cnvkit_samples["Sample_".$sample->sample_id] = $sample->case_id;
+								if (count($glsamples) > 0) {
+									$cnvkit_genelevel_samples[$sample->sample_id] = $sample->case_id;
+								}
+							}
 						}
-						else
+						else {
 							$cnvkit_samples[$sample->sample_id] = $sample->case_id;
+							if (count($glsamples) > 0) {
+								$cnvkit_genelevel_samples[$sample->sample_id] = $sample->case_id;
+							}
+						}
 					} else {
 						Log::info("CNVkit file exists: $file");
 						$cnvkit_samples[$sample->sample_name] = $sample->case_id;
+						if (count($glsamples) > 0) {
+							$cnvkit_genelevel_samples[$sample->sample_name] = $sample->case_id;
+						}
 					}
 					$file = storage_path()."/ProcessedResults/".$sample->path."/$patient_id/$sample->case_id/$sample->sample_name/cnvTSO/$sample->sample_name".".cns";					
 					if (!file_exists($file)) {
